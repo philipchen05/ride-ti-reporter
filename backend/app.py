0ba1fbb58e1ff1e340a6e6ef6257e8d88479d0ba
Philip <philipchen935@gmail.com>
2024-07-16 13:25:55 -0400

    Initialized Flask backend

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hola, Flask!"
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route("/login", methods=['POST'])
+def login():
+    return "yo"
