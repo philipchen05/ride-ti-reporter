55b7dacc413c602a62b8f717a8c9bdf216f7873d
Philip <philipchen935@gmail.com>
2024-09-15 19:24:50 -0400

    Redirected API endpoints to backend

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -24,7 +24,7 @@ export default function LoginPage() {
         const formData = new FormData(event.currentTarget)
         const username = formData.get('username')
         const password = formData.get('password')
-        const response = await fetch('http://127.0.0.1:5000/login', {
+        const response = await fetch('https://ride-ti-reporter-backend-r2tsfeja2q-nn.a.run.app/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password }),
